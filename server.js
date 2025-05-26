86afbe3415de42ac74798455c7165ce633b3fed6
Sonali Nachankaar <80123687+sonali168@users.noreply.github.com>
2025-05-26 22:11:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('MongoDB connected'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection failed:', err));
+
 
 app.use('/api/cart', cartRoutes);
 
